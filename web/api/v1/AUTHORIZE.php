05a551a05f499b1b2393da5fbd88356970ed1c2a
Brendan Connelly <brendanjconnelly17@gmail.com>
2025-01-14 16:58:31 -0500

    Database is now persistent across docker-compose redeployments

--- web/api/v1/AUTHORIZE.php
+++ web/api/v1/AUTHORIZE.php
@@ -5,10 +5,11 @@ require_once getenv("PHP_ROOT") . "/api/helper/USER_AUTH.php";
 // THIS FILE CANNOT BE USED FOR PURE BACKEND TOKEN AUTHENTICATION
 
 $args = json_decode(file_get_contents('php://input'));
-if(!(isset($args->username) || !isset($args->token))) {
+if(!(isset($args->username) || !isset($args->token)) || $args->username === "" || $args->token === "") {
     echo '10';
     exit();
 }
+
 $username = $args->username;
 $token = $args->token;
 
